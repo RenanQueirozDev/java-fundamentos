12b4674eaec01675672941d5a59389155058dc91
RenanQueirozDev <Renanmacielgta2@gmail.com>
2026-08-26 03:57:56 -0300

    chore: remove unnecessary comments

--- src/javacore/accessModifiers/domain/BankAccount.java
+++ src/javacore/accessModifiers/domain/BankAccount.java
@@ -1,12 +1,5 @@
 package javacore.accessModifiers.domain;
 
-/*
-Create a BankAccount class with a private balance attribute (double).
-Add a getBalance() and a setBalance() that only accepts values greater
-than or equal to zero (it doesn't allow a negative balance).
-*/
-
-
 public class BankAccount {
     private double balance;
 
