9f33ab977d3d8c30f4e68e3eaee387876e3960be
RenanQueirozDev <Renanmacielgta2@gmail.com>
2026-08-28 01:13:23 -0300

    feat: add static attribute counter example with School class

---
+++ src/javacore/statiicStudies/domain/School.java
@@ -0,0 +1,9 @@
+package javacore.statiicStudies.domain;
+
+public class School {
+    public static int countStudents;
+
+    public School() {
+        countStudents++;
+    }
+}
---
+++ src/javacore/statiicStudies/test/SchoolTest.java
@@ -0,0 +1,10 @@
+package javacore.statiicStudies.test;
+
+import javacore.statiicStudies.domain.School;
+
+public class SchoolTest {
+  public static void main(String[] args) {
+      new School();
+       System.out.println(School.countStudents);
+    }
+}
